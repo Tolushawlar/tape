ea9af4109d10013b21f18dc2ff4847e8574603e8
Tolushawlar <Tolushaw.it@gmail.com>
2025-05-27 17:11:21 +0100

    category update

--- src/components/CollectionSection.tsx
+++ src/components/CollectionSection.tsx
@@ -30,7 +30,7 @@ const CollectionSection = ({
         <p className="text-[18px] font-[700] tracking-wider">
           {collectionName}
         </p>
-        {showExploreButton && (
+        {showExploreButton && itemsData.length > 0 && (
           <HoverTextWithArrow
             text="Browse"
             textColor="text-black"
@@ -38,24 +38,28 @@ const CollectionSection = ({
           />
         )}
       </div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center w-full max-w-[1280px]">
-        {itemsData.map((item, index) => (
-          <ImageCard
-            id={item.id}
-            key={index}
-            name={item.name}
-            defaultImage={item.defaultImage}
-            hoverImage={item.hoverImage}
-            color1={item.color1}
-            price={item.price}
-            size1={item.size1}
-            size2={item.size2}
-            size3={item.size3}
-            size4={item.size4}
-            size5={item.size5}
-          />
-        ))}
-      </div>
+      {itemsData.length > 0 ? (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center w-full max-w-[1280px]">
+          {itemsData.map((item, index) => (
+            <ImageCard
+              id={item.id}
+              key={index}
+              name={item.name}
+              defaultImage={item.defaultImage}
+              hoverImage={item.hoverImage}
+              color1={item.color1}
+              price={item.price}
+              size1={item.size1}
+              size2={item.size2}
+              size3={item.size3}
+              size4={item.size4}
+              size5={item.size5}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-2xl font-bold mt-8">Coming Soon</p>
+      )}
     </div>
   );
 };
